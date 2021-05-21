b1d7a2a5ce7b2acf929ec99e27524156fbc2e637
Abdulaziz Almekhlafi <Abdulazuz716111@gmail.com>
2021-05-21 21:21:35 +0300

    login and logout problem was solved.

--- src/project/settings.py
+++ src/project/settings.py
@@ -133,3 +133,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/product'
+LOGOUT_REDIRECT_URL = '/product'
