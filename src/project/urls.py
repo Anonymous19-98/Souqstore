efd0842bd845c65aba968dda06d8a8bf8e324018
Abdulaziz Almekhlafi <Abdulazuz716111@gmail.com>
2021-05-22 20:24:30 +0300

    login and sigh up and profile are almost done.

--- src/project/urls.py
+++ src/project/urls.py
@@ -20,9 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('product.urls', namespace='products')),
-    # path('product/', include('product.urls', namespace='products')),
+    path('products/', include('product.urls', namespace='products')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+
 
 ]
 
